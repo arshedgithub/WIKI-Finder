32e7094c586a2489b3da2f2089bebcba69ef7a34
arshedgithub <arshedahmedmail@gmail.com>
2021-04-17 00:02:07 +0530

    styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,26 @@
 const form = document.querySelector('form');
-const ul = document.querySelector('ul');
+const list = document.querySelector('.list');
 
-const updateUI = async (data) => {
+const updateUI = (data) => {
 
     const searchResultArray = data.query.search;
-    // console.log(data.query.search);
+
     for (let i = 0; i < searchResultArray.length; i++) {
-        ul.innerHTML += `
-        <li><h3>${searchResultArray[i].title}</h3><p>${searchResultArray[i].snippet}</p></li>
+        list.innerHTML += `
+        <div class="items" id='${searchResultArray[i].pageid}' style="padding: 2vh 2vh 2vh 4vh;">
+        <h3>${searchResultArray[i].title}</h3>
+        <p>${searchResultArray[i].snippet}</p>
+        </div>
         `;
-        console.log(searchResultArray[i]);
     }
-}
+
+};
 
 form.addEventListener('keyup', e => {
     // prevent default action
     e.preventDefault();
-    
-    ul.innerHTML = ``;
+
+    list.innerHTML = ``;
 
     const keyWord = form.search.value.trim();
 
@@ -31,4 +34,19 @@ form.addEventListener('submit', e => {
     e.preventDefault();
 
     return;
+});
+
+list.addEventListener('click', e => {
+    let id;
+    const { target } = e;
+    console.log(target.getAttribute("class") == 'items');
+
+    if (target.getAttribute("class") == 'items') {
+        id = target.getAttribute("id");
+    } else {
+        id = target.parentElement.getAttribute("id");
+    }
+    const page = `https://en.wikipedia.org/?curid=${id}`;
+    window.open(page, "_blank");
+
 });
